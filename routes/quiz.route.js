62525c7bd5eea321560d211c54155a87c965a142
Konark-2ez <konarkkumar456@gmail.com>
2023-07-17 14:14:15 +0530

    added id

--- routes/quiz.route.js
+++ routes/quiz.route.js
@@ -20,5 +20,14 @@ quizRouter.get("/get",async(req,res)=>{
         res.status(400).send({"msg":error.message})
     }
 })
+quizRouter.get("/get/:id",async(req,res)=>{
+
+    try {
+        const quiz = await quizModel.findOne({_id:req.params.id})
+        res.status(200).send(quiz)
+    } catch (error) {
+        res.status(400).send({"msg":error.message})
+    }
+})
 
 module.exports = {quizRouter}
